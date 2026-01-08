e7d8e32172a0aaa71330c970ab7ad067b0f0bab6
Abhi-Wathore <abhishekbwathore@gmail.com>
2026-01-08 16:49:02 +0530

    id update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  'https://ihgzgokxxkxfidmdryuw.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImloZ3pnb2t4eGt4ZmlkbWRyeXV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc2Njc2MTAsImV4cCI6MjA4MzI0MzYxMH0.BonFy7PKFLmd5n4BeOg7gmhMpBeLad18MhFxDDeQRuc'
+)
